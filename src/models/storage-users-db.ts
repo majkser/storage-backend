02e1fddb06198f3307d7131d23956dae20c3a6e2
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-03-20 16:44:10 +0100

    fix: names

--- src/models/storage-users-db.ts
+++ src/models/storage-users-db.ts
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
-const connection = mysql.createPool({
+const usersDbConnection = mysql.createPool({
   host: "localhost:3306",
   user: "root",
   password: process.env.DB_PASSWORD,
   database: "StorageDB",
 });
 
-export default connection;
+export default usersDbConnection;
