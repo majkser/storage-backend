f08a29c11075a6bb78a3951161f4da97445db9b0
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-03-21 01:02:38 +0100

    fix: connection with db

--- src/config/storage-users-db.ts
+++ src/config/storage-users-db.ts
@@ -1,7 +1,8 @@
 import mysql from "mysql2/promise";
 
 const usersDbConnection = mysql.createPool({
-  host: "localhost:3306",
+  host: "localhost",
+  port: 3306,
   user: "root",
   password: process.env.DB_PASSWORD,
   database: "StorageDB",
