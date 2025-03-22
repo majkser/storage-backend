71c5523d0dc33b92a0706f0cd7b98293339534c1
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-03-22 12:13:36 +0100

    fix: db

---
+++ src/config/db-connection.ts
@@ -0,0 +1,14 @@
+import mysql from "mysql2/promise";
+
+const dbConnection = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT as string),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+export default dbConnection;
