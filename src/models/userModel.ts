c156233bb21625bce2baca717651ab9c9f2c1da1
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-03-23 17:15:16 +0100

    feat: added func that gives ability go get user from db via googleID

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,7 +1,8 @@
 import dbConnection from "../config/db-connection";
 
 export interface User {
-  id: number;
+  id?: number;
+  googleId?: string;
   userName: string;
   userSurname: string;
   email: string;
@@ -9,11 +10,14 @@ export interface User {
 
 export async function createUser(user: User): Promise<void> {
   const connection = await dbConnection.getConnection();
-  await connection.execute(
-    "INSERT INTO Users (username, usersurname, email) VALUES (?, ?, ?)",
-    [user.userName, user.userSurname, user.email]
-  );
-  connection.release();
+  try {
+    await connection.execute(
+      "INSERT INTO Users (google_id, username, usersurname, email) VALUES (?, ?, ?, ?)",
+      [user.googleId, user.userName, user.userSurname, user.email]
+    );
+  } finally {
+    connection.release();
+  }
 }
 
 export async function getUserByEmail(email: string): Promise<User | null> {
@@ -22,6 +26,25 @@ export async function getUserByEmail(email: string): Promise<User | null> {
     const [rows]: [any[], any] = await connection.execute(
       "SELECT * FROM Users WHERE email = ?",
       [email]
+    );
+
+    if (Array.isArray(rows) && rows.length === 0) {
+      return null;
+    }
+    return rows[0] as User;
+  } finally {
+    connection.release();
+  }
+}
+
+export async function getUserByGoogleId(
+  googleId: string
+): Promise<User | null> {
+  const connection = await dbConnection.getConnection();
+  try {
+    const [rows]: [any[], any] = await connection.execute(
+      "SELECT * FROM Users WHERE google_id = ?",
+      [googleId]
     );
 
     if (Array.isArray(rows) && rows.length === 0) {
