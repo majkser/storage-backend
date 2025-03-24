6a16189000ad513cff8f673539af830786726e5c
vect0r <vectoreddd>
2025-03-24 22:25:46 +0100

    fixes according to pull request review

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,14 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
-// tworzenie basenu (ang. pool creation)
-export const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+const dbConnection = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT as string),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
+
+export default dbConnection;
--- src/models/file.model.ts
+++ src/models/file.model.ts
@@ -1,16 +1,16 @@
-import { pool } from '../config/database';
+import  dbConnection  from '../config/database';
 import { RowDataPacket, ResultSetHeader } from 'mysql2';
 
 export interface File {
-    id?: number;
+    id: number;
     filename: string;
-    original_name: string;
-    file_path: string;
+    originalName: string;
+    filePath: string;
     size: number;
     mimetype: string;
-    user_id: number;
-    parent_folder_id?: number | null;
-    is_public?: boolean;
-    created_at?: Date;
-    updated_at?: Date;
+    userId: number;
+    parentFolderId?: number | null;
+    isPublic: boolean;
+    createdAt: Date;
+    updatedAt?: Date;
 }
