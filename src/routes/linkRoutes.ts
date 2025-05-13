e7dce748093b68ba8da2afcd6631e475649e8d56
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-05-14 01:07:58 +0200

    feat: added first simple validations

--- src/routes/linkRoutes.ts
+++ src/routes/linkRoutes.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import { generateLink, getFileByLink } from "../controllers/linkController";
+import { body } from "express-validator";
+import validateRequest from "../middleware/requestValidator.middleware";
 
 const router = express.Router();
 
-router.post("/generate-link", generateLink);
+//TODO add middleware to check if user is authenticated for generating link
+
+router.post(
+  "/generate-link",
+  body("fileId").toInt().notEmpty().isInt({ min: 1 }),
+  validateRequest,
+  generateLink
+);
 router.get("/:token", getFileByLink);
 
 export default router;
