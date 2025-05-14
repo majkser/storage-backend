fe200993004ce8e5bbbad81fe1a37eb56432d96c
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-05-14 14:08:44 +0200

    feat: added more validation

--- src/routes/linkRoutes.ts
+++ src/routes/linkRoutes.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { generateLink, getFileByLink } from "../controllers/linkController";
-import { body } from "express-validator";
-import validateRequest from "../middleware/requestValidator.middleware";
+import { body, param } from "express-validator";
+import validateRequest from "../middlewares/requestValidator.middleware";
+import fileExistanceValidator from "../middlewares/fileExistanceValidator.middleware";
 
 const router = express.Router();
 
@@ -9,10 +10,27 @@ const router = express.Router();
 
 router.post(
   "/generate-link",
-  body("fileId").toInt().notEmpty().isInt({ min: 1 }),
+  body("fileId")
+    .toInt()
+    .notEmpty()
+    .withMessage("fileId is required")
+    .isInt({ min: 1 }),
   validateRequest,
+  fileExistanceValidator,
   generateLink
 );
-router.get("/:token", getFileByLink);
+
+router.get(
+  "/:token",
+  param("token")
+    .notEmpty()
+    .withMessage("token is requierd")
+    .isLength({ min: 36, max: 36 })
+    .withMessage("token length is not valid")
+    .matches(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/)
+    .withMessage("token format is not valid"),
+  validateRequest,
+  getFileByLink
+);
 
 export default router;
