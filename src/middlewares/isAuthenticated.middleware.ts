0b4caa94e23442b640e836ac24c9922683f048dd
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-05-17 12:05:27 +0200

    feat: added Auth connected middlewares

---
+++ src/middlewares/isAuthenticated.middleware.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+
+export default function isAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.status(401).json({ error: "Unauthorized" });
+}
