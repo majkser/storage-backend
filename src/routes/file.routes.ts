49cd417bf1c3667c262dac40d826e4c054e84422
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-07-08 18:03:54 +0200

    fix: requestValidator

--- src/routes/file.routes.ts
+++ src/routes/file.routes.ts
@@ -1,16 +1,27 @@
-import express from 'express';
-import { Request, Response, NextFunction } from 'express';
-import { uploadFile, getFiles, downloadFile, removeFile, getFileData } from '../controllers/file';
-import { upload } from '../config/multerconf';
-import isAuthenticated from '../middlewares/isAuthenticated.middleware';
-import requestValidator from '../middlewares/requestValidator.middleware';
-import { fetchFileData } from '../middlewares/fileFetch.middleware';
+import express from "express";
+import {
+  uploadFile,
+  getFiles,
+  downloadFile,
+  removeFile,
+  getFileData,
+} from "../controllers/file";
+import { upload } from "../config/multerconf";
+import isAuthenticated from "../middlewares/isAuthenticated.middleware";
+import requestValidator from "../middlewares/requestValidator.middleware";
+import { fetchFileData } from "../middlewares/fileFetch.middleware";
 const router = express.Router();
 
-router.post('/upload', isAuthenticated, upload.array('file'), uploadFile, requestValidator);
-router.get('/userfiles', isAuthenticated, getFiles, requestValidator);
-router.get('/:id/download', downloadFile, requestValidator);
-router.delete('/:id/delete', isAuthenticated, removeFile, requestValidator);
-router.get('/:id', fetchFileData ,getFileData, requestValidator);
+router.post(
+  "/upload",
+  isAuthenticated,
+  upload.array("file"),
+  requestValidator,
+  uploadFile
+);
+router.get("/userfiles", isAuthenticated, requestValidator, getFiles);
+router.get("/:id/download", requestValidator, downloadFile);
+router.delete("/:id/delete", isAuthenticated, removeFile, requestValidator);
+router.get("/:id", requestValidator, fetchFileData, getFileData);
 
 export default router;
